c2407bb843d0de6e0ce58cf2c9ac03f83e090750
Zavodsk0y <zimniyvolk5@gmail.com>
2025-01-14 12:20:24 +0700

    task-2

---
+++ day-two/task-2/index.ts
@@ -0,0 +1,65 @@
+// Представьте, что у вас есть данные в одном формате, а вашему приложению требуется работать 
+// с этими данными в другом формате. Реализуйте функцию, которая выполняет преобразование данных.
+// Алгоритм:
+// Принять данные в исходном формате (например, строка, список, словарь и т.д.).
+// Преобразовать эти данные в нужный формат.
+// Вернуть преобразованные данные.
+
+interface SourceData {
+    user_id: string,
+    full_name: string,
+    contact: {
+        phone: string,
+        email: string,
+    },
+    address: {
+        city: string,
+        zip: string,
+    },
+    is_active: boolean
+};
+
+interface TargetData {
+    id: string,
+    name: string,
+    phone: string,
+    email: string,
+    location: string,
+    status: string
+}
+
+const source: SourceData = {
+    user_id: "123",
+    full_name: "John Doe",
+    contact: {
+        phone: "+123456789",
+        email: "john.doe@example.com",
+    },
+    address: {
+        city: "New York",
+        zip: "10001",
+    },
+    is_active: true,
+};
+
+const target: TargetData = {
+    id: "123",
+    name: "John Doe",
+    phone: "+123456789",
+    email: "[john.doe@example.com](<mailto:john.doe@example.com>)",
+    location: "New York, 10001",
+    status: "active",
+};
+
+const convertData = (sourceData: SourceData): TargetData => {
+    return {
+        id: sourceData.user_id,
+        name: sourceData.full_name,
+        phone: sourceData.contact.phone,
+        email: `[${sourceData.contact.email}](<mailto:${sourceData.contact.email}>)`,
+        location: `${sourceData.address.city}, ${sourceData.address.zip}`,
+        status: sourceData.is_active ? "active" : "not_active"
+    }
+}
+
+console.log(convertData(source))
