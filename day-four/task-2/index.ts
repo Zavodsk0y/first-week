e8a979b6dd180a90776304953da912d5e075d62d
Zavodsk0y <zimniyvolk5@gmail.com>
2025-01-16 17:22:24 +0700

    refactor with map

--- day-four/task-2/index.ts
+++ day-four/task-2/index.ts
@@ -1,28 +1,28 @@
 // Дано n объектов одного типа
-type UserWithGadget = {
-	id: string,
-	name: string,
-	gadget: {
-		id: string,
-		name: string,
-		price: string | undefined
-	}
+type UserWithGadgetType = {
+    id: string,
+    name: string,
+    gadget: {
+        id: string,
+        name: string,
+        price: string | undefined
+    }
 }[]
 
 // Ваша задача привести весь массив объектов типа userWithGadget[] к типу userWithGadgets. 
 // То есть, необходимо сгруппировать гаджеты по их владельцам, чтобы в итоговом массиве каждому пользователю соответствовал список всех его гаджетов.
 
-interface UserWithGadgets {
-	id: string,
-	name: string,
-	gadgets: {
-		id: string,
-		name: string,
-		price: string | undefined
-	}[]
+interface IUserWithGadgets {
+    id: string,
+    name: string,
+    gadgets: {
+        id: string,
+        name: string,
+        price: string | undefined
+    }[]
 }
 
-const input: UserWithGadget = [
+const input: UserWithGadgetType = [
     {
         id: "1",
         name: "Khalid Kashmiri",
@@ -40,25 +40,24 @@ const input: UserWithGadget = [
     },
 ];
 
-// Пробовал ковырять reduce, но в итоге всего этого решил вернуться к простым циклам
-const groupUsersGadgets = (users: UserWithGadget): UserWithGadgets[] => {
-    const grouped: Record<string, UserWithGadgets> = {}
-    for(let user of users) {
-        // Здесь чутка "херим", добавляя лишние ключи по айдишнику, по типу "1" и т.д.
-        if (!grouped[user.id]) {
-            grouped[user.id] = {
-                // Спредом в этом случае не воспользуюсь, так как гаджет будет закидываться
-                id: user.id,
-                name: user.name,
-                gadgets: [user.gadget]
-            }
-        } else {
-            // Если пользователь с таким ключом/айди уже есть, то просто запушим с него гаджет туда
-            grouped[user.id].gadgets.push(user.gadget)
+const groupUsersGadgets = (users: UserWithGadgetType) => {
+    // Теперь используем мапу, ибо в ходе написания понял, что есть удобные методы, по типу set, has
+    const userMap = new Map();
+
+    for (let user of users) {
+        // Извлечем из юзера данные
+        const {id, name, gadget} = user;
+        // Проверяем, есть ли в мапе юзер с таким ключом (айди также в этом случае)
+        if (!userMap.has(id)) {
+            // В случае отсутствия закидываем юзера в мапу с этим ключом, и меняем соответственно структуру, добавляя лишь gadgets
+            userMap.set(id, { id, name, gadgets: [] });
         }
+        // Далее добавляем в массив гаджетов непосредственно гаджет
+        userMap.get(id)!.gadgets.push(gadget);
     }
-    // При помощи Object.values() избавимся от "1", "2", лишних значений, которые не должны присутствовать в выводе
-    return Object.values(grouped)
+
+    // Не совсем ожидаемое использование спред-оператора, однако код выше выглядит наиболее лаконично, нежели предыдущий
+    return [...userMap.values()];
 }
 
 console.log(groupUsersGadgets(input))
